6524f8f59e941a0ac48533eed98611f240bf94e7
andreim2021 <andrei.marchevca@paynova.com>
2024-03-07 18:09:49 +0100

    feat: details comp get data

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.scss'
+  styleUrl: './details.component.scss',
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  pokemon: any;
+  name: string | null = null;
 
+  constructor(
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.name = this.activatedRoute.snapshot.paramMap.get('name');
+    this.getPokemonDetails();
+    console.log('OI', this.pokemon);
+  }
+
+  getPokemonDetails(): void {
+    this.apiService
+      .get<any>(`pokemon/${this.name}`)
+      .subscribe((pokemon) => (this.pokemon = pokemon));
+  }
 }
