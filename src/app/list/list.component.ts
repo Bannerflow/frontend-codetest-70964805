f9e5630ae87ec93678247a2be583d86b01ef4c47
andreim2021 <andrei.marchevca@paynova.com>
2024-03-07 17:09:52 +0100

    feat: list component basic

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { PokemonListItem } from '../pokemon.list.item';
+import { CommonModule } from '@angular/common';
+import { List } from '../list';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss',
+})
+export class ListComponent implements OnInit {
+  pokemonList: PokemonListItem[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.getPokemonList();
+    console.log(this.pokemonList);
+  }
+
+  getPokemonList(): void {
+    this.apiService
+      .get<List>('pokemon')
+      .subscribe((pokemon) => (this.pokemonList = pokemon.results));
+  }
+}
