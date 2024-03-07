f028799f93560bea44e3f2b0da136e4f8005dedd
andreim2021 <andrei.marchevca@paynova.com>
2024-03-07 17:12:18 +0100

    feat: routing module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'pokemon-list' },
+  { path: 'pokemon-list', component: ListComponent },
+  { path: 'details/:name', component: DetailsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
