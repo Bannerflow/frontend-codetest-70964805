765c89d6b5a915771494d6b0d43b63dafa1f09bc
andreim2021 <andrei.marchevca@paynova.com>
2024-03-07 17:13:15 +0100

    fix: just routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,9 @@
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { DetailsComponent } from './details/details.component';
-import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'pokemon-list' },
-  { path: 'pokemon-list', component: ListComponent },
-  { path: 'details/:name', component: DetailsComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'pokemon-list' },
+    { path: 'pokemon-list', component: ListComponent },
+    { path: 'details/:name', component: DetailsComponent },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
